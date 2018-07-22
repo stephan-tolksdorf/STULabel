6f63aa10a0f93549f85821aa9e1f9e1947b439a5
Stephan Tolksdorf <st@quanttec.com>
2018-07-22 17:24:47 +0200

    Optimize NSCoderUtils

--- STULabel/Internal/NSCoderUtils.hpp
+++ STULabel/Internal/NSCoderUtils.hpp
@@ -6,7 +6,7 @@ namespace stu_label {
 
 template <typename T, EnableIf<isIntegral<T>> = 0>
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, T value) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key, T value) {
   if constexpr (isSame<T, bool>) {
     [coder encodeBool:value forKey:key];
   } else if constexpr (sizeof(T) <= 4 && (isSigned<T> || sizeof(T) < 4)) {
@@ -19,7 +19,7 @@ void encode(NSCoder* coder, NSString* key, T value) {
 
 template <typename T, EnableIf<isIntegral<T>> = 0>
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<T> value) {
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key, Out<T> value) {
   if constexpr (isSame<T, bool>) {
     value = [coder decodeBoolForKey:key];
   } else if constexpr (sizeof(T) <= 4 && (isSigned<T> || sizeof(T) < 4)) {
@@ -32,89 +32,113 @@ void decode(NSCoder* coder, NSString* key, Out<T> value) {
 
 template <typename T, EnableIf<isEnum<T>> = 0>
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, T value) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key, T value) {
   encode(coder, key, static_cast<UnderlyingType<T>>(value));
 }
 template <typename T, EnableIf<isEnum<T>> = 0>
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<T> outValue) {
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Out<T> outValue)
+{
   UnderlyingType<T> t;
   decode(coder, key, Out{t});
   outValue = static_cast<T>(t);
 }
 
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, Float32 value) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key, Float32 value) {
   [coder encodeFloat:value forKey:key];
 }
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<Float32> outValue) {
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Out<Float32> outValue)
+{
   outValue = [coder decodeFloatForKey:key];
 }
 
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, Float64 value) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key, Float64 value) {
   [coder encodeDouble:value forKey:key];
 }
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<Float64> outValue) {
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Out<Float64> outValue)
+{
   outValue = [coder decodeDoubleForKey:key];
 }
 
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, CGSize value) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key, CGSize value) {
   [coder encodeCGSize:value forKey:key];
 }
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<CGSize> outValue) {
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Out<CGSize> outValue)
+{
   outValue = [coder decodeCGSizeForKey:key];
 }
 
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, CGRect value) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key, CGRect value) {
   [coder encodeCGRect:value forKey:key];
 }
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<CGRect> outValue) {
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Out<CGRect> outValue)
+{
   outValue = [coder decodeCGRectForKey:key];
 }
 
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, Rect<CGFloat> value) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Rect<CGFloat> value)
+{
   [coder encodeCGRect:value forKey:key];
 }
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<Rect<CGFloat>> outValue) {
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Out<Rect<CGFloat>> outValue)
+{
   outValue = [coder decodeCGRectForKey:key];
 }
 
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, UIEdgeInsets value) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            UIEdgeInsets value)
+{
   [coder encodeUIEdgeInsets:value forKey:key];
 }
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<UIEdgeInsets> outValue) {
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Out<UIEdgeInsets> outValue)
+{
   outValue = [coder decodeUIEdgeInsetsForKey:key];
 }
 
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, NSObject<NSSecureCoding>* object) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            NSObject<NSSecureCoding>* object)
+{
   if (object) {
     [coder encodeObject:object forKey:key];
   }
 }
 template <typename T, EnableIf<isConvertible<T*, NSObject<NSSecureCoding>*>> = 0>
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<T*> outObject) {
-  outObject = down_cast<T*>([coder decodeObjectOfClass:[T self] forKey:key]);
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Out<T*> outObject)
+{
+  outObject = [coder decodeObjectOfClass:[T class] forKey:key];
 }
 
 STU_INLINE
-void encode(NSCoder* coder, NSString* key, NSRange value) {
+void encode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key, NSRange value) {
   encode(coder, key, [NSValue valueWithRange:value]);
 }
 STU_INLINE
-void decode(NSCoder* coder, NSString* key, Out<NSRange> outValue) {
+void decode(NSCoder* __unsafe_unretained coder, NSString* __unsafe_unretained key,
+            Out<NSRange> outValue)
+{
   NSValue* value = nil;
   decode(coder, key, Out{value});
   outValue = value.rangeValue;
