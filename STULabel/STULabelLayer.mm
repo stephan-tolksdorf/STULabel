b8bb669bef150388fc44da64cd8b00baed716104
Stephan Tolksdorf <st@quanttec.com>
2018-09-17 14:21:18 +0200

    Fix bug in LabelLayer.updateTextFrameInfo introduced by recent commit

--- STULabel/STULabelLayer.mm
+++ STULabel/STULabelLayer.mm
@@ -780,7 +780,7 @@ private:
     } else {
       // This function is also called by createTextFrameIfNecessary, so we have to ensure here that
       // textFrame_ is nonnull even if textFrameInfoIsValidForCurrentSize_.
-      if (!textFrame_) {
+      if (!textFrameInfoIsValidForCurrentSize_ || !textFrame_) {
         if (task_ && task_->tryCopyLayoutInfoTo(*this)) return;
         if (!shapedString_) {
           updateAttributedStringIfNecessary();
