7ccd3d2c863cd77ae1118a99805df9a953b16d11
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-14 09:18:49 +0800

    Delete Demo directory

--- Demo/Demo-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2018 Stephan Tolksdorf
-
-#import "TextFramePerformanceVC-Drawing.h"
-
-void CGContextSetBaseCTM(CGContextRef c, CGAffineTransform ctm);
-
-
-@interface UIFont (STU_UIFont_NSCoding) <NSSecureCoding>
-@end
--- Demo/Utils/UIEdgeInsetsExtension.swift
+++
@@ -1,9 +0,0 @@
-// Copyright 2018 Stephan Tolksdorf
-
-import UIKit
-
-extension UIEdgeInsets {
-  init(uniformInset: CGFloat) {
-    self.init(top: uniformInset, left: uniformInset, bottom: uniformInset, right: uniformInset)
-  }
-}
